85fb73f229897bc6dbe8fc9356dbd06b7b3e04be
Francy Yuliana Zapata <francyzapata119@hotmail.com>
2016-11-30 16:08:06 -0500

    datasource
    se coloco el apuntador hacia el jar de postgres y el war para que
    desplegara automaticamente los deloymente y el datasource

--- Datasource.sh
+++ Datasource.sh
@@ -1,10 +1,55 @@
 #!/bin/bash
+
+# Usage: execute.sh [WildFly mode] [configuration file]
+#
+# The default mode is 'standalone' and default configuration is based on the
+# mode. It can be 'standalone.xml' or 'domain.xml'.
+
+JBOSS_HOME=/opt/jboss/wildfly
+JBOSS_CLI=$JBOSS_HOME/bin/jboss-cli.sh
+JBOSS_MODE=${1:-"standalone"}
+JBOSS_CONFIG=${2:-"$JBOSS_MODE.xml"}
+
+function wait_for_server() {
+  until `$JBOSS_CLI -c ":read-attribute(name=server-state)" 2> /dev/null | grep -q running`; do
+    sleep 1
+  done
+}
+
+echo "=> Starting WildFly server"
+$JBOSS_HOME/bin/$JBOSS_MODE.sh -b 0.0.0.0 -c $JBOSS_CONFIG &
+$JBOSS_HOME/bin/$JBOSS_MODE.sh -bmanagement 0.0.0.0 -c $JBOSS_CONFIG &
+
+echo "=> Waiting for the server to boot"
+wait_for_server
+
+$JBOSS_CLI -c << EOF
 batch
-module add --name=org.postgres --resources=/opt/jboss/wildfly/standalone/deployments/postgresql-9.4.1212.jre6.jar --dependencies=javax.api,javax.transaction.api
 
-/subsystem=datasources/jdbc-driver=postgres:add(driver-name=postgres,driver-module-name=org.postgresql.Driver)
+set CONNECTION_URL=jdbc:postgresql://104.198.170.207:5432/Arquitectura
 
-data-source add --jndi-name=java:/servicioDS --name=servicioDS --connection-url=jdbc:postgresql://localhost:5432/Arquitectura --driver-name=postgres --user-name=postgres --password=12345
- 
+# Add postgres module
+module add --name=org.postgres --resources=https://github.com/francy119/arquitectura/blob/master/postgresql-9.4.1212.jre6.jar --dependencies=javax.api,javax.transaction.api
+
+# Add postgres driver
+/subsystem=datasources/jdbc-driver=mysql:add(driver-name=mysql,driver-module-name=com.mysql,driver-xa-datasource-class-name=com.mysql.jdbc.jdbc2.optional.MysqlXADataSource)
+
+# Add the datasource
+data-source add --jndi-name=java:/servicioDS --name=servicioDS --connection-url=jdbc:postgresql://localhost:5432/Arquitectura?useUnicode=true&amp;characterEncoding=UTF-8 ---driver-name=postgres --user-name=postgres --password=12345 --use-ccm=false --max-pool-size=25 --blocking-timeout-wait-millis=5000 --enabled=true
+
+# Execute the batch
 run-batch
+EOF
 
+# Deploy the WAR
+cp https://github.com/francy119/arquitectura/blob/master/SolicitarServicios.war $JBOSS_HOME/$JBOSS_MODE/deployments/SolicitarServicios.war
+
+echo "=> Shutting down WildFly"
+if [ "$JBOSS_MODE" = "standalone" ]; then
+  $JBOSS_CLI -c ":shutdown"
+else
+  $JBOSS_CLI -c "/host=*:shutdown"
+fi
+
+echo "=> Restarting WildFly"
+$JBOSS_HOME/bin/$JBOSS_MODE.sh -b 0.0.0.0 -c $JBOSS_CONFIG
