98d86d3ead00fd0024d3e59dbc21345ce36b7df2
Francy Yuliana Zapata <francyzapata119@hotmail.com>
2016-11-30 14:42:47 -0500

    bash

--- Datasource.sh
+++ Datasource.sh
@@ -1,5 +1,5 @@
+#!/bin/bash
 batch
-
 module add --name=org.postgres --resources=/opt/jboss/wildfly/standalone/deployments/postgresql-9.4.1212.jre6.jar --dependencies=javax.api,javax.transaction.api
 
 /subsystem=datasources/jdbc-driver=postgres:add(driver-name=postgres,driver-module-name=org.postgresql.Driver)
